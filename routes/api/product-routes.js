0fd70c7b035990c6757f4ae517d7d8f3d22ee891
Alkarias <alkarias29@gmail.com>
2022-08-21 08:18:58 -0400

    finished product routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,7 +108,11 @@ router.put('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
   try {
-    let productData;
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
     res.status(200).json(productData);
   } catch(err) {
     res.status(500).json(err);
